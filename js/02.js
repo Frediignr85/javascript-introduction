a6b2383a4c9f4b5c581adccfda451be7ed65cd34
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2023-05-18 21:21:03 -0600

    Variables const

---
+++ js/02.js
@@ -0,0 +1,11 @@
+// Variables con const (No se pueden reasignar)
+// No se puede inicializar sin ningun valor
+
+const nombre = 'Fredy';
+const apellido = 'Benitez';
+const precioCompra = 230.
+
+// Impresion de datos
+console.log(`Nombre: ${nombre}`);
+console.log(`Apellido: ${apellido}`);
+console.log(`Precio de compra: ${precioCompra}`);
