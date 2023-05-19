5616a9c76488db450412b6e88a6bb8320bdc77cf
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2023-05-18 21:17:45 -0600

    Variables con Let

---
+++ js/01.js
@@ -0,0 +1,15 @@
+//Variables con let
+
+//Asignacion de valores con let
+let cliente = 'Fredy';
+let precioCompra = 14.25;
+
+//Varibles asignadas
+let fecha;
+// Reasignacion
+fecha = '10-02-2023';
+
+
+console.log(cliente);
+console.log(precioCompra);
+console.log(fecha);
