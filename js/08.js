d04d1ed748a33cba9dfa3a82effbda8351f9d915
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2023-05-22 21:30:35 -0600

    Arrays

---
+++ js/08.js
@@ -0,0 +1,16 @@
+// Arrays o Arreglos
+
+const tecnologias = [
+    20,
+    30, 
+    true,
+    'React',
+    'Javascript'
+];
+
+console.log(tecnologias);
+console.table(tecnologias);
+//Accediendo a un valor de un arreglo
+console.log(tecnologias[3]);
+//Imprimiendolo como string
+console.log(tecnologias.toString())
