3d010d7306f1cef50ae838c8ab233dff02d6f512
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2023-05-22 21:53:34 -0600

    Destructuring arrays

---
+++ js/10.js
@@ -0,0 +1,7 @@
+//Destructuring arrays
+
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+
+const [html, css, javascript, react, node] = tecnologias;
+
+console.log(html, css, javascript, react, node);
